20de83302483d9f1b07d08aec64adc3feac1ec9a
manohar-12 <k.manoharyadav26@gmail.com>
2023-01-23 17:57:08 +0530

    assignment 23-01-23

--- PythonAssignments/Palindrome_or_Not.py
+++ PythonAssignments/Palindrome_or_Not.py
@@ -4,6 +4,24 @@ palindrome
 
 , and false otherwise.'''
 
+#without Conversion
+def Palindrome(n):
+    if n < 0:
+        return False
+    else:
+        l=n
+        res=0
+        while n >0:
+            k = n% 10
+            res = (res*10)+k
+            n=n//10
+    if res == l:
+        return True
+    else: return False
+print(Palindrome(121))
+
+
+#with Conversion
 def Palindeome(input1):
     if str(input1)==str(input1)[::-1]:
         return True
