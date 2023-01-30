622381d94a0d689ef55f86fb77ed48e27c2d54a6
manohar-12 <k.manoharyadav26@gmail.com>
2023-01-30 18:23:32 +0530

    Assignment

--- PythonAssignments/Assignment27-01-23.py
+++ PythonAssignments/Assignment27-01-23.py
@@ -10,8 +10,9 @@
     ]
 }Based on the Configuration in driver.json your program will execute.
 Write ShellScript/Python script to read and execute the program.'''
-    
-    
+
+
+   
 import json
 import os
 f= open('driver.json')
@@ -19,3 +20,9 @@ data=json.load(f)
 for i in range(len(data['app'])):
     k=data['app'][i]['program']
     os.system('python {}'.format(k))
+    
+    
+    
+    
+
+    
